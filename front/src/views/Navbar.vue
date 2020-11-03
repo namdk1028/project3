6f4f428a2ebd2c8426b6b38d099fa225d2492f29
kimnayu <mung0605@naver.com>
2020-11-03 16:53:46 +0900

    [F] feat: navbar buttons

--- front/src/views/Navbar.vue
+++ front/src/views/Navbar.vue
@@ -2,17 +2,17 @@
   <div class="navbar">
       <div class="container-navbar">
           <div class="navbar-item">
-              <button class="navbar-btn">
+              <button class="navbar-btn" @click="moveToMain">
                 <img class="navbar-btn-img" src="@/assets/images/navbar/home.png" alt="">
               </button>
           </div>
           <div class="navbar-item">
-                            <button>
+              <button class="navbar-btn" @click="moveToChat">
                 <img class="navbar-btn-img" src="@/assets/images/navbar/chat.png" alt="">
               </button>
           </div>
           <div class="navbar-item">
-              <button>
+              <button class="navbar-btn" @click="moveToProfile">
                 <img class="navbar-btn-img" src="@/assets/images/navbar/user.png" alt="">
               </button>
           </div>
@@ -23,6 +23,17 @@
 <script>
 export default {
     name: "Navbar",
+    methods: {
+      moveToMain() {
+        this.$router.push("/main")
+      },
+      moveToChat() {
+        this.$router.push("/message")
+      },
+      moveToProfile() {
+        this.$router.push("/profile")
+      },
+    }
 }
 </script>
 
