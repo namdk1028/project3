f8d8152bfc25d5b07f23343a3dbacec42c57c45a
unknown <namdk7@naver.com>
2020-11-12 17:31:26 +0900

    [F] fix

--- front/src/main.js
+++ front/src/main.js
@@ -5,9 +5,8 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import VueCookie from "vue-cookie";
 import axios from "axios";
-import Vue2TouchEvents from 'vue2-touch-events'
- 
 import io from 'socket.io-client';
+
 const socket = io('http://k3a507.p.ssafy.io:3000');
 // const socket = io('http://localhost:3000');
 
@@ -17,7 +16,6 @@ Vue.prototype.$socket = socket;
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
 Vue.use(VueCookie);
-Vue.use(Vue2TouchEvents)
 
 new Vue({
   router,
