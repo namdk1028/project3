675bf3b005c3b2f675e4f3da4a2e4fd12b543eb4
kimnayu <mung0605@naver.com>
2020-11-13 18:53:02 +0900

    [F] feat: profile picture update modal

--- front/src/components/profile/ProfilePic.vue
+++ front/src/components/profile/ProfilePic.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="container-profilepic">
-      <div class="profilepic-background"></div>
+      <div class="profilepic-background" @click="closeModal"></div>
       <div class="profilepic-modal">
+          <i @click="closeModal" class="fas fa-times btn-close"></i>
           <div class="modal-header">
             프로필 사진 변경
           </div>
           <div class="modal-body">
-              <div class="upload-img" @click="clickUploadBtn">
-                  <label for="uploadedImg"><i class="far fa-file-image"></i></label>
-                  <input type="file" accept="image/png, image/jpeg" id="uploadedImg" @change="previewImg(upload-preview)">
+              <div class="similarity">유사도 %</div>
+              <div class="upload-img">
+                  <div class="upload-preview" @click="clickUploadBtn"></div>
+                  <label><i class="far fa-file-image"></i></label>
+                  <input type="file" accept="image/png, image/jpeg" id="uploadedImg" @change="previewUpload">
               </div>
-              <div class="camera-img" @click="clickCameraBtn">
+              <div class="border"></div>
+              <div class="camera-img">
+                  <div class="camera-preview" @click="clickCameraBtn"></div>
                   <label for="cameraImg"><i class="fas fa-camera"></i></label>
-                  <input type="file" id="cameraImg" @change="previewImg(camera-preview)">
+                  <input type="file" accept="image/png, image/jpeg" id="cameraImg" @change="previewCamera">
               </div>
           </div>
       </div>
@@ -25,23 +30,35 @@ export default {
     name: "ProfilePic",
     data() {
         return {
-            uploadedImg: null,
-            cameraImg: null,
             uploadedfile: "",
             uploadedURL: "",
+            camerafile: "",
+            cameraURL: "",
         }
     },
     methods: {
+        closeModal() {
+            document.querySelector(".container-profilepic").classList.add("hide")
+        },
         clickUploadBtn() {
-            console.log("dkfhd")
             document.querySelector("#uploadedImg").click();
         },
-        previewFile(event, className) {
-            var preview = document.querySelector(`.${className}`);
+        clickCameraBtn() {
+            document.querySelector("#cameraImg").click();
+        },
+        previewUpload(event) {
+            var preview = document.querySelector('.upload-preview');
             this.uploadedfile = event.target.files[0];
-            this.uploadedURL = URL.createObjectURL(this.uploadedfile);
-            preview.style.backgroundImage = `url('${this.url})`;
-        }
+            this.uploadedURL = URL.createObjectURL(event.target.files[0]);
+            preview.style.backgroundImage = `url('${this.uploadedURL}')`;
+        },
+        previewcamera(event) {
+            var preview = document.querySelector('.camera-preview');
+            this.camerafile = event.target.files[0];
+            this.cameraURL = URL.createObjectURL(event.target.files[0]);
+            preview.style.backgroundImage = `url('${this.cameraURL}')`;
+        },
+
     }
 }
 </script>
